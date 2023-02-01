2a1013ca9b42706d6c22f19a7b23308680a3a2d4
Adriana <Adriana>
2023-02-01 17:14:45 -0500

    partA

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,13 @@
-
+#PartA
+weeks = 16
+classes = 4
+tuition = 6000
+cost_per_week = ((tuition/classes)/weeks)
+print("Cost per week:", cost_per_week)
+classes_per_week = 12
+cost_per_class = cost_per_week/classes_per_week
+print("Cost per class:", cost_per_class)
+print("Number of weeks:", weeks)
+print("Classes:", classes)
+print("Tuition:", tuition)
+print("Classes per week:", classes_per_week)
