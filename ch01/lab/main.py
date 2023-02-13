9c46537f9251e12c47cda374103399eff4786ac7
Adriana <Adriana>
2023-02-13 16:48:33 -0500

    yes

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,16 +1,16 @@
 #PartA
 weeks = 16
+print("Number of weeks", type(weeks))
 classes = 4
+print("Number of classes", type(classes))
 tuition = 6000
+print("tuition",type(tuition))
 cost_per_week = ((tuition/classes)/weeks)
-print("Cost per week:", cost_per_week)
+print("Cost per week:", type(cost_per_week))
 classes_per_week = 12
+print("Classes per week", type(classes_per_week))
 cost_per_class = cost_per_week/classes_per_week
-print("Cost per class:", cost_per_class)
-print("Number of weeks:", weeks)
-print("Classes:", classes)
-print("Tuition:", tuition)
-print("Classes per week:", classes_per_week)
+print("Cost per class:", type(cost_per_class))
 #PartB
 import random
 mylist=["yellow","banana","alien","cyan","panda"]
