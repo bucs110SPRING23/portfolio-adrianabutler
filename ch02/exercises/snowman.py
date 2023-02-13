bcf911a6b2508ac7ad04e6c418a96f98215c3295
Adriana <Adriana>
2023-02-13 18:01:23 -0500

    snowman

---
+++ ch02/exercises/snowman.py
@@ -0,0 +1,18 @@
+while 1:
+    pygame.event.get()
+    import pygame
+    pygame.init()
+
+    screen=pygame.display.set_mode()
+    screen.fill("gray")
+    pygame.display.flip
+
+    screen_size=screen.get_size()
+
+    pygame.draw.circle(screen,"white",[screen_size[0]/2,200],50)
+    pygame.draw.circle(screen, "white", [screen_size[0]/2,280], 75)
+    pygame.draw.circle(screen,"white",[screen_size[0]/2,390],100)
+
+    pygame.display.flip()
+    pygame.time.wait(2500)
+    break
--- ch02/lab/main.py
+++
@@ -1,6 +0,0 @@
-import pygame
-pygame.init()
-screen=pygame.display.set_mode()
-screen_size=screen.get_size()
-dimensions=[screen_size[0]/2, screen_size[1]/2,250,250]
-pygame.draw.rect(screen,"black", dimensions)
