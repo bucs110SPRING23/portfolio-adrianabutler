b6a507d1bdcf4b11f437f6d028c23158dfaafdb7
Adriana <Adriana>
2023-02-22 16:57:17 -0500

    guess

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,12 +1,10 @@
 import random
-x = random.randrange(1,11)
+num = random.randrange(1,11)
 for i in range(3):
     guess = int(input("Guess the number 1 through 10:"))
-    if guess < x:
-        print("Too Low")
-    elif guess > x:
-        print("Too High")
-    else:
-        print("Correct!!")
-    exit()
-exit()
+if guess == num:
+    print("You guessed it! My number was", num)
+elif guess < num:
+    print("Too Low!")
+elif guess > num:
+    print("Too High!")
