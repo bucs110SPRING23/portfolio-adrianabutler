5015eebcd10e19d4839bccf7cb65e6fe0c508bb8
Adriana <Adriana>
2023-03-06 18:06:45 -0500

    pyramid

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,5 +1,12 @@
-def star_pyramid():
-    layers = int(input("Enter the number of layers:"))
-    for i in range(layers):
-        print((i+1)*"*")
+def star_pyramid(p):
+    for _ in range(p+1):
+        print("*" * _ )
 
+star_pyramid(int(input("Enter a number to determine the amount of layers in your pyramid: ")))
+
+def rstar_pyramid(p):
+    for _ in range(p+1):
+        print("*" * p)
+        p -=1
+
+rstar_pyramid(int(input("Enter a number to determine the amount of layers in your pyramid: ")))
