bd3e7e7a6eaeccd0c8ef2215e2771afa5fed5308
Adriana <Adriana>
2023-05-05 23:31:55 -0400

    lab5 fix

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,45 +1,56 @@
 import pygame
 
 def threenp1(n):
+    while n > 1.0:
+        if n % 2 == 0:
+            n = int(n/2)
+        else:
+            n = int(3 * n + 1)
+    return None
+
+def threenp1count(n):
     count = 0
     while n > 1.0:
         count = count + 1
         if n % 2 == 0:
-            n = int(n / 2)
+            n = int(n/2)
         else:
-            n = int(3*n+1)
-            while n == 1.0:
-                return count
+            n = int(3 * n + 1)
+    if n == 1:
+        count = count + 1
+    return count
 
-def threenp1range(upperlimit):
-    threenplus1_dictionary = {}
-    for n in range(2, upperlimit+1):
-        count = threenp1(n)
-        threenplus1_dictionary = count
-        return threenplus1_dictionary
-
-def graph_coordinates(threeplus1_iters_dict):
-    pygame.init
-    while 1:
-        pygame.event.pump()
-        display = pygame.display.set_mode()
-        pygame.draw.lines(display, "black", False, list(threeplus1_iters_dict.items()))
-        dictionary = list(threeplus1_iters_dict.items())
-        max_so_far = 0
-        for i in range(len(dictionary)):
-            if (dictionary[i][1] > max_so_far):
-                max_so_far = dictionary[i][1]
-        new_display = pygame.transform.flip(display, False, False)
-        display.blit(new_display, (0, 0))
-        font = pygame.font.Font(None, 48)
-        msg = font.render(("Max so far is:" + str(max_so_far)), False, "aquamarine")
-        display.blit(msg, (1200, 10))
-        pygame.display.flip()
-        break
-    pygame.time.wait(1000)
+def threenp1range(upper_limit):
+    objs_in_sequence = {}
+    for _ in range(2, upper_limit + 1):
+        result = threenp1count(_)
+        objs_in_sequence.update({_ : result})
+    return objs_in_sequence
 
 def main():
-    print(threenp1range(20))
-    graph_coordinates(threenp1range(20))
+    result = threenp1range(20)
+    print(result)
+    graph_coordinates(result)
 
+def graph_coordinates(d):
+    startpoint = (0, 0)
+    pygame.init()
+    screen = pygame.display.set_mode()
+    max_so_far = 0
+    for k, v in d.items():
+        pygame.draw.line(screen, "red", startpoint, (k * 20, v * 20))
+        startpoint = (k * 20 , v * 20)
+    for k, v in d.items():
+        if v > max_so_far:
+            max_so_far = v
+    width, height = screen.get_size()
+    newdisplay = pygame.transform.scale(screen, (width , height))
+    newdisplay = pygame.transform.flip(newdisplay, False, True)
+    screen.blit(newdisplay, (0, 0))
+    font = pygame.font.Font(None, 48)
+    message = str(max_so_far)
+    msg = font.render("The max is " + message + " iterations!", True, "blue")
+    screen.blit(msg, (10, 10))
+    pygame.display.flip()
+    pygame.time.wait(5000)
 main()
